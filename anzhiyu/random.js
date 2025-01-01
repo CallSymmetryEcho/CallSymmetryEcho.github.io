2804f695f21eaf31dfcc57596e3688a75cc744d8
jakie_lian <lianbin522957@gmail.com>
2024-12-31 22:01:39 -0600

    dec31up3

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/27/hello-world/","2024/02/17/奥斯汀晚饭乱找挑战1-- Bao'd up/","2024/02/17/乐音解忧（1） like a star/","2024/02/17/步进电机工作原理及使用/","2024/02/17/hexo_优化图片插入使用 hexo-image-link/","2024/12/27/读书笔记/论语第一--学而/","2024/02/17/这里是常有的杂想和脑袋的备份/"];function toRandomPost(){
+var posts=["2024/12/27/hello-world/","2024/02/17/乐音解忧（1） like a star/","2024/02/17/奥斯汀晚饭乱找挑战1-- Bao'd up/","2024/12/27/读书笔记/论语第一--学而/","2024/02/17/hexo_优化图片插入使用 hexo-image-link/","2024/02/17/步进电机工作原理及使用/","2024/02/17/这里是常有的杂想和脑袋的备份/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
